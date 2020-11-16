6d566511ba6ba570ec71b5abce54cafe0a01c899
Tobias Belch <t.belch@ehealth-tec.de>
2020-11-16 14:54:14 +0100

    adds pdf-lib pdf without metadata

--- pdfbox/src/main/java/evaluation/pdfbox/Main.java
+++ pdfbox/src/main/java/evaluation/pdfbox/Main.java
@@ -7,13 +7,14 @@ import java.io.*;
 import java.nio.file.Paths;
 
 public class Main {
-  private static final String[] filePaths = new String[] { "valid.pdf" };
+  private static final String[] filePaths = new String[] { "valid.pdf", "pdf-lib.no-metadata.pdf" };
 
   public static void main(String[] args) {
     for (String filePath : filePaths) {
       try {
-        System.out.println(filePath);
-        readMetadata(Paths.get("resources", filePath).toFile());
+        File file = Paths.get("resources", filePath).toFile();
+        System.out.println(filePath + " -> " + file.getAbsolutePath());
+        readMetadata(file);
       } catch (Exception e) {
         System.err.println("Caught error while reading");
         e.printStackTrace(System.err);
@@ -24,7 +25,6 @@ public class Main {
   private static void readMetadata(File pdfContent) {
     if (pdfContent.exists()) {
       try (PDDocument doc = PDDocument.load(pdfContent)) {
-        System.out.println(pdfContent.getAbsolutePath());
         System.out.println("  information: " + doc.getDocumentInformation());
         System.out.println("  version: " + doc.getVersion());
         PDMetadata metadata = doc.getDocumentCatalog().getMetadata();
