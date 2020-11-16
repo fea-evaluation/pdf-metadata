252ce2cefb6eb3307435843291709ce184c96850
Tobias Belch <t.belch@ehealth-tec.de>
2020-11-16 14:48:49 +0100

    adds valid pdf

--- pdfbox/src/main/java/evaluation/pdfbox/Main.java
+++ pdfbox/src/main/java/evaluation/pdfbox/Main.java
@@ -4,27 +4,36 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.common.PDMetadata;
 
 import java.io.*;
+import java.nio.file.Paths;
 
 public class Main {
-    private static final String[] filePaths = new String[]{"valid_swisscom.pdf", "af__added_xpacket.pdf"};
+  private static final String[] filePaths = new String[] { "valid.pdf" };
 
-    public static void main(String[] args) {
-        for (String filePath : filePaths) {
-            try {
-                System.out.println(filePath);
-                readMetadata(Main.class.getClassLoader().getResourceAsStream(filePath));
-            } catch (Exception e) {
-                System.err.println("Caught error while reading");
-                e.printStackTrace(System.err);
-            }
-        }
+  public static void main(String[] args) {
+    for (String filePath : filePaths) {
+      try {
+        System.out.println(filePath);
+        readMetadata(Paths.get("resources", filePath).toFile());
+      } catch (Exception e) {
+        System.err.println("Caught error while reading");
+        e.printStackTrace(System.err);
+      }
     }
+  }
 
-    private static void readMetadata(InputStream pdfContent) throws IOException {
-        PDDocument doc = PDDocument.load(pdfContent);
-        System.out.println("  information: "+ doc.getDocumentInformation());
-        System.out.println("  version: "+ doc.getVersion());
+  private static void readMetadata(File pdfContent) {
+    if (pdfContent.exists()) {
+      try (PDDocument doc = PDDocument.load(pdfContent)) {
+        System.out.println(pdfContent.getAbsolutePath());
+        System.out.println("  information: " + doc.getDocumentInformation());
+        System.out.println("  version: " + doc.getVersion());
         PDMetadata metadata = doc.getDocumentCatalog().getMetadata();
-        System.out.println("  metadata: "+ metadata);
+        System.out.println("  metadata: " + metadata);
+      } catch (Exception e) {
+        e.printStackTrace(System.err);
+      }
+    } else {
+      System.out.println(pdfContent.getAbsolutePath());
     }
+  }
 }
