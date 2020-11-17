ae206861ea231abd3004f4cbc94ae6120759fe13
Tobias Belch <t.belch@ehealth-tec.de>
2020-11-17 10:59:42 +0100

    reads jspdf pdfs

--- pdfbox/src/main/java/evaluation/pdfbox/Main.java
+++ pdfbox/src/main/java/evaluation/pdfbox/Main.java
@@ -7,12 +7,13 @@ import java.io.*;
 import java.nio.file.Paths;
 
 public class Main {
-  private static final String[] filePaths = new String[] { "valid.pdf", "no-metadata.pdf-lib.pdf" };
+  private static final String[] filePaths = new String[] { "valid.pdf", "no-metadata.pdf-lib.pdf",
+      "no-metadata.jspdf.pdf", "metadata.jspdf.pdf" };
 
   public static void main(String[] args) {
     for (String filePath : filePaths) {
       try {
-        File file = Paths.get("resources", filePath).toFile();
+        File file = Paths.get("..","resources", filePath).toFile();
         System.out.println();
         System.out.println(filePath + " -> " + file.getAbsolutePath());
         readMetadata(file);
